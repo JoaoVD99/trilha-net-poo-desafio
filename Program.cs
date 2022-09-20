bfc8349e82daa802c281a9f0a09cb4633ad488c5
João Vítor <dortajoao9991@gamil.com>
2022-09-20 18:48:20 -0300

    Desafio de POO concluído

--- Program.cs
+++ Program.cs
@@ -1,3 +1,12 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Um Nokia de fato");
+Nokia nokia = new Nokia("448888-8888", "Nokia joguin cobrinha", "xxx-1111", "2GB");
+nokia.Ligar();
+nokia.ReceberLigacao();
+
+Console.WriteLine("Um Iphone de fato");
+Iphone iphone = new Iphone("449999-9999", "Iphone 15", "yyy-2222", "256GB");
+iphone.Ligar();
+iphone.ReceberLigacao();
